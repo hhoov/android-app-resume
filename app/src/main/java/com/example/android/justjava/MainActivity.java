89923fa86fc09b49f569f3c769015fc28f08f5f3
Hannah Hoover <hannahghoover@gmail.com>
2018-03-13 18:35:42 -0500

    Screwed up a commit, but I'm fairly certain this is the latest change. Email button working. Need to have signed in on the device to have the message composed though. Working on that.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,15 +8,10 @@ package com.example.android.justjava;
  * in the project's AndroidManifest.xml file.
  **/
 
-
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import java.util.List;
+import android.view.View;
 
 /**
  * This app displays an order form to order coffee.
@@ -24,31 +19,45 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     String subject = "Internship";
     String body = "Hi Hannah,\n Great work! When would you like to start your internship?";
-
+    String recipient = "user@fakehost.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sendEmail();
+        //onClickEmail();
     }
 
-
-
+    /**
+     * This method is called when the order button is clicked.
+     */
+/*
+    public void onClickEmail() {
+        sendEmail();
+    }
+*/
     /**
      * This method opens email application and fills out subject and body.
      */
-    public void sendEmail() {
+    public void sendEmail(View view) {
 
         // Build the intent
-        Uri data = Uri.parse("mailto:recipient@example.com?subject=" + subject + "&body=" + body);
-        Intent emailIntent = new Intent(Intent.ACTION_MAIN);
-        emailIntent.addCategory(Intent.CATEGORY_APP_EMAIL);
-        emailIntent.setData(data);
+        //Uri data = Uri.parse("mailto:recipient@example.com?subject=" + subject + "&body=" + body);
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        //emailIntent.addCategory(Intent.CATEGORY_APP_EMAIL);
+        //emailIntent.setData(data);
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, recipient);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.setType("plain/text");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+
+
+        startActivity(Intent.createChooser(emailIntent, "Send your email in:"));
         //emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {"jon@example.com"}); // recipients
         //emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email subject");
         //emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message text");
-
+/*
         // Verify the app exists to handle the intent
         PackageManager packageManager = getPackageManager();
         List<ResolveInfo> activities = packageManager.queryIntentActivities(emailIntent, 0);
@@ -58,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         if (isIntentSafe) {
             startActivity(emailIntent);
         }
+            */
         /*
         Intent intent = new Intent(Intent.ACTION_MAIN);
         intent.addCategory(Intent.CATEGORY_APP_EMAIL);
