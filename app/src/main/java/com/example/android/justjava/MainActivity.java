d33021179d295d88cfcddb6f9bc7e5d54a50dbd4
Hannah Hoover <hannahghoover@gmail.com>
2018-03-13 13:15:09 -0500

    Playing around with layouts and views. Still trying to display pdf.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,15 +9,19 @@ package com.example.android.justjava;
  **/
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.TextView;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    String subject = "Internship";
+    String body = "Hi Hannah,\n Great work! When would you like to start your internship?";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +32,37 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method is called when the order button is clicked.
      */
-    public void submitOrder(View view) {
-        display(1);
+    /*
+    public void onClickEmail(View view) {
+        sendEmail();
     }
-
+*/
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method opens email application and fills out subject and body.
+     */
+    public void sendEmail() {
+
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+        Uri data = Uri.parse("mailto:recipient@example.com?subject=" + subject + "&body=" + body);
+        intent.setData(data);
+        startActivity(intent);
+        /*
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+        startActivity(intent);
+        */
+        /*
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.android.email");
+        startActivity(intent);
+        */
     }
 }
