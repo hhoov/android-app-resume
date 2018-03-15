a36362b6435cbab9af7e38dc2b4ac678f705581f
Hannah Hoover <hannahghoover@gmail.com>
2018-03-15 12:23:08 -0500

    Fixed an error not showing available email apps able to perform the email action. Accidentally uncommented the addCategory() line.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,25 +9,42 @@ package com.example.android.justjava;
  **/
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
-import java.util.List;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    String subject = "Internship";
+    String body = "Hi Hannah,\nGreat work! When would you like to start your internship?";
+    String[] recipient = { "hannahghoover@gmail.com" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //onClickEmail();
+
+/*
+        Button emailButton = (Button)v.findViewById(R.id.email_button);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_EMAIL, R.array.email_recipient);
+                i.putExtra(Intent.EXTRA_TEXT, R.string.body_text);
+                i.putExtra(Intent.EXTRA_SUBJECT, R.string.subject_text);
+                startActivity(i);
+            } });
+        return v;
+*/
+
+
     }
+
+
 
     /**
      * This method opens email application and fills out subject and body.
@@ -37,17 +54,17 @@ public class MainActivity extends AppCompatActivity {
         // Build the intent
         //Uri data = Uri.parse("mailto:recipient@example.com?subject=" + subject + "&body=" + body);
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.addCategory(Intent.CATEGORY_APP_EMAIL);
+        //emailIntent.addCategory(Intent.CATEGORY_APP_EMAIL);
         //emailIntent.setData(data);
 
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, R.array.email_recipient);
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.subject_text);
-        emailIntent.putExtra(Intent.EXTRA_TEXT, R.string.body_text);
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, recipient);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_TEXT, body);
 
         emailIntent.setType("plain/text");
         startActivity(Intent.createChooser(emailIntent, "Send your email in:"));
-
-
+        //startActivity(emailIntent);
+/*
         // Verify the app exists to handle the intent
         PackageManager packageManager = getPackageManager();
         List<ResolveInfo> activities = packageManager.queryIntentActivities(emailIntent, 0);
@@ -57,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         if (isIntentSafe) {
             startActivity(Intent.createChooser(emailIntent, "Send your email in:"));
         }
-
+*/
         /*
         Intent intent = getPackageManager().getLaunchIntentForPackage("com.android.email");
         startActivity(intent);
