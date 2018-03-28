a52e47523f60dfb25205b64fc98bcb40eeecd338
Hannah Hoover <hannahghoover@gmail.com>
2018-03-28 13:00:25 -0500

    Trial and error in trying to get the hamburger menu icon to show. I'm wondering if I'm erring with having seemingly 2 app bars...

--- app/src/main/java/com/example/android/justjava/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/android/justjava/NavigationDrawerActivity.java
@@ -57,6 +57,7 @@ public class NavigationDrawerActivity extends AppCompatActivity
                     }
                 }
         );
+
 /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +77,22 @@ public class NavigationDrawerActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         */
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
     }
 
     @Override
