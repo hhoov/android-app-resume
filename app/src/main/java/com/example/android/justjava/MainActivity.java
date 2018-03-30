73e8ff6078e0ae648661d17f816d828fd49852dd
Hannah Hoover <hannahghoover@gmail.com>
2018-03-30 12:03:32 -0500

    Undid a few additions.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,24 +8,26 @@ package com.example.android.justjava;
  * in the project's AndroidManifest.xml file.
  **/
 
+import android.os.Bundle;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
-import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
+
 
 import java.util.List;
+
 
 /**
  * This app displays a resume, email button, and navigation drawer.
@@ -123,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
                 }
         });
 
-    }
+    } // onCreate()
 
 /*    private ActionBarDrawerToggle setupDrawerToggle() {
         // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
