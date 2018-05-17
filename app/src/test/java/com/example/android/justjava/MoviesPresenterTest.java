ffefc33454b2520065c7f953707aff737dae92c9
Hannah Hoover <hannahghoover@gmail.com>
2018-05-17 12:24:01 -0500

    Presenter should not be mocked. When testing the presenter, I'm also really testing the view as well. So my tests here cover MoviesView's methods as well as presenter's methods. In setup() I now initialize a new executor and presenter, as well as go ahead and attach the mocked view to the presenter.
    shouldGetMovies() creates a dummy arraylist with dummy data that is then returned by mocked MDP's getMovieData(). Presenter presents, and I verify that the view never shows an error and then that it shows movies as set.
    
    shouldShowErrorTest() throws an exception when setMovies(empty arraylist) is called on the mocked view. Verifies movies are never set/shown and that it does show error.

--- app/src/test/java/com/example/android/justjava/MoviesPresenterTest.java
+++ app/src/test/java/com/example/android/justjava/MoviesPresenterTest.java
@@ -1,108 +1,61 @@
 package com.example.android.justjava;
 
+import com.example.android.justjava.data.MovieDataProvider;
 import com.example.android.justjava.model.MovieData;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
 
-import okhttp3.OkHttpClient;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.mockito.ArgumentMatchers.anyListOf;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MoviesPresenterTest {
 
-    @Mock(name = "Movies View") private MoviesPresenter.MoviesView moviesView;
-    @Mock(name = "Movies Presenter") private MoviesPresenter presenter;
-    @Mock(name = "Movie Data") private List<MovieData> movieData;
-    // Could I use @Inject here for okHttp?
-    private OkHttpClient okClient = new OkHttpClient();
-    private OkhttpHelper ok = new OkhttpHelper(okClient);
-    //private MoviesPresenter presenter = new MoviesPresenter(ok);
+    @Mock(name = "Movies View") private MoviesPresenter.MoviesView mockMoviesView;
+    @Mock(name = "Movie Data Provider") private MovieDataProvider mockMovieDataProvider;
+
+    @InjectMocks
+    private MoviesPresenter presenter;
+    private DirectExecutor executor;
 
     @Before
-    public void init() {
+    public void setup() {
         MockitoAnnotations.initMocks(this);
+        executor = new DirectExecutor();
+        presenter = new MoviesPresenter(mockMovieDataProvider, executor, executor);
+        presenter.attach(mockMoviesView);
     }
-    @Ignore
+
     @Test
-    public void shouldAttachTest() {
-        assertNull(presenter.view);
+    public void shouldGetMovies() throws IOException {
+        ArrayList<MovieData> movieData = new ArrayList<>();
+        movieData.add(new MovieData(12,"eh", 2, "89", 45, 34, "fes",""));
 
-        presenter.attach(moviesView);
-        assertNotNull(presenter.view);
-    }
-    @Ignore
-    @Test
-    public void shouldDetachTest() {
-        presenter.attach(moviesView);
-        assertNotNull(presenter.view);
-
-        presenter.detach();
-        assertNull(presenter.view);
-
-    }
-
-    @Ignore
-    @Test(expected = MockitoException.class)
-    public void shouldPresent() {
+        when(mockMovieDataProvider.getMovieData()).thenReturn(movieData);
         presenter.present();
-        verify(moviesView).setMovies(movieData);
-
+        verify(mockMoviesView, never()).showError();
+        verify(mockMoviesView).setMovies(movieData);
     }
 
-    @Ignore
-    @Test
-    public void shouldSetMoviesTest() {
-        // Replace with mock
-        // String url = "https://raw.githubusercontent.com/MercuryIntermedia/Sample_Json_Movies/master/top_movies.json";
-        // String jsonData = ok.makeRequest(mock(url));
-
-        // Fix
-        //when(ok.makeRequest()).thenReturn("http://someJsonUrl.json");
-        //when(presenter).thenReturn();
-
-        int expectedMoviesListSize = 100;
-        List<MovieData> testOutput = null;
-
-        /*try {
-            testOutput = MovieDataProvider.getInstance(jsonData).getMovieData();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-        assertFalse(testOutput.isEmpty());
-        presenter.attach(moviesView);
-        presenter.view.setMovies(testOutput);
-        assertEquals(expectedMoviesListSize, testOutput.size());
-    }
-
-    @Ignore
-    @Test
-    public void shouldSetMoviesInThreadTest() {
-        // TODO
-        // To test this...maybe:
-        // Have two threads, main and worker
-        // Make main wait while worker sets movies in background (?)
-        // Wait until condition satisfied before continuing...?
-    }
-
-    @Ignore
     @Test(expected = MockitoException.class)
     public void shouldShowErrorTest() {
-        doThrow(new IOException()).when(moviesView).setMovies(movieData);
-        verify(moviesView).showError();
+        doThrow(new IOException()).when(mockMoviesView).setMovies(new ArrayList<MovieData>());
+        verify(mockMoviesView, never()).setMovies(anyListOf(MovieData.class));
+        verify(mockMoviesView).showError();
+
     }
 
 }
