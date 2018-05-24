1a30af0ae77fad5db0a90ea5346c808b52121e5e
Hannah Hoover <hannahghoover@gmail.com>
2018-05-24 17:44:12 -0500

    Started ProgressPresenterTest

---
+++ app/src/test/java/com/example/android/justjava/ProgressPresenterTest.java
@@ -0,0 +1,25 @@
+package com.example.android.justjava;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProgressPresenterTest {
+    @Mock(name = "Movies View") private ProgressPresenter.ProgressView mockProgressView;
+    @Mock(name = "Progress Provider") private ProgressProvider mockProgressProvider;
+
+    @InjectMocks
+    private ProgressPresenter presenter;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        presenter = new ProgressPresenter(mockProgressProvider);
+        presenter.attach(mockProgressView);
+    }
+
+}
