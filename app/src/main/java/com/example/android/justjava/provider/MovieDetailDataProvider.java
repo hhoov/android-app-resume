2fdc55fd6fa56d7a756acea7e95044a41409d6c0
Hannah Hoover <hannahghoover@gmail.com>
2018-06-25 15:20:27 -0500

    WIP -- Added provider for movie detail data; looks the same as MovieDataProvider, so if those two data files are merged, MDDP will no longer be required as MDP will provide for all data.

---
+++ app/src/main/java/com/example/android/justjava/provider/MovieDetailDataProvider.java
@@ -0,0 +1,57 @@
+package com.example.android.justjava.provider;
+
+import com.example.android.justjava.JSONParser;
+import com.example.android.justjava.model.MovieDetailData;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@Singleton
+public class MovieDetailDataProvider {
+    private String jsonData;
+    private String url = "https://raw.githubusercontent.com/MercuryIntermedia/Sample_Json_Movies/master/top_movies.json";
+    private JSONParser parser = new JSONParser();
+
+    private final OkHttpClient okHttpClient;
+
+    // Private constructor prevents any other class from instantiating.
+    @Inject
+    MovieDetailDataProvider(OkHttpClient okHttpClient) {
+        this.okHttpClient = okHttpClient;
+    }
+
+    // Call readDetailJsonStream with response string arg on JSONParser object
+    public List<MovieDetailData> getMovieDetailData() throws IOException {
+        jsonData = makeRequest(url);
+        return parser.readDetailJsonStream(jsonData);
+    }
+
+    private String makeRequest(String url) {
+
+        try {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            Response response = null;
+            try {
+                response = okHttpClient.newCall(request).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            jsonData = response.body().string();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonData;
+    }
+}
