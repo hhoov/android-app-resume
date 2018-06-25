aa8280adf08d6375210409891788b98f3228cbee
Hannah Hoover <hannahghoover@gmail.com>
2018-06-25 15:17:09 -0500

    Added separate readJsonStream method to return list of type MovieDetailData. If the two models are merged, would just need to delete this second method.

--- app/src/main/java/com/example/android/justjava/JSONParser.java
+++ app/src/main/java/com/example/android/justjava/JSONParser.java
@@ -1,6 +1,7 @@
 package com.example.android.justjava;
 
 import com.example.android.justjava.model.MovieData;
+import com.example.android.justjava.model.MovieDetailData;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,5 +19,14 @@ public class JSONParser {
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         // readValue deserializes JSON content from given JSON content String into Java object (using generics)
         return mapper.readValue(response, new TypeReference<List<MovieData>>(){});
+    }
+
+    public List<MovieDetailData> readDetailJsonStream(String response) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Config deserialization
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        // readValue deserializes JSON content from given JSON content String into Java object (using generics)
+        return mapper.readValue(response, new TypeReference<List<MovieDetailData>>(){});
     }
 }
