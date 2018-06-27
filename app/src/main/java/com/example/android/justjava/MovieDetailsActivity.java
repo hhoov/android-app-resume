40792b4d5d4edd4b367d7f4d11238c9692dd723d
Hannah Hoover <hannahghoover@gmail.com>
2018-06-27 16:52:02 -0500

    WIP -- will revisit once MovieSummaryPresenter is further along.

--- app/src/main/java/com/example/android/justjava/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/justjava/MovieDetailsActivity.java
@@ -6,16 +6,13 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.example.android.justjava.model.MovieDetailData;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
-    private ImageView poster;
-    private TextView rating, votes, id, title, year, rated, released, runtime, genre;
-    private TextView director, writer, actors, plot, language, country, awards, metascore;
+    //private ImageView poster;
+    //private TextView rating, votes, id, title, year, rated, released, runtime, genre;
+    //private TextView director, writer, actors, plot, language, country, awards, metascore;
 
     public MovieDetailsActivity() {
 
@@ -26,42 +23,19 @@ public class MovieDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        poster = (ImageView) findViewById(R.id.posterImageView);
-        title = (TextView) findViewById(R.id.titleTextView);
-        id = (TextView) findViewById(R.id.imdbIdTextView);
+        //MovieDetailData movieDetails = (MovieDetailData) getIntent().getExtras().getSerializable("imdbId");
+
+        //poster = (ImageView) findViewById(R.id.posterImageView);
+        //TextView title = (TextView) findViewById(R.id.titleTextView);
+        TextView id = (TextView) findViewById(R.id.imdbIdTextView);
 
         Intent intent = getIntent();
         String action = intent.getAction();
         Uri data = intent.getData();
 
-        MovieDetailData movieDetailData = (MovieDetailData) intent.getExtras().getSerializable("movie_details");
-
-        /*Intent results = getIntent();
-        //intent.getExtras();
-
-        String id = results.getStringExtra(textView.toString());
-
-        textView.setText(id);*/
+        //title.setText(movieDetails.getTitle());
+        //id.setText(movieDetails.getImdbId());
 
     }
-
-/*    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.nav_movies_grid:
-                //finish();
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-            case R.id.nav_movies_list:
-                //finish();
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-            case R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
-
 
 }
