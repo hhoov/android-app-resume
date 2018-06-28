0792b1e8c5e860bb62e5028d9806073aa8294f94
Hannah Hoover <hannahghoover@gmail.com>
2018-06-28 12:22:09 -0500

    Changed MSP's constructor to take in a MovieData object instead of just the imdbId. onMovieClicked() and displayMovieDetail(imdbId) are still WIP, but I think the idea is that I'd be able to call the getters for each attribute of the MovieData object and pass them through the Intent to start the new details activity.

--- app/src/main/java/com/example/android/justjava/MovieSummaryPresenter.java
+++ app/src/main/java/com/example/android/justjava/MovieSummaryPresenter.java
@@ -1,6 +1,9 @@
 package com.example.android.justjava;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.android.justjava.model.MovieData;
 
 /**
  * An observer/listener of changes. Presenter for item Views & download progress.
@@ -10,35 +13,50 @@ public class MovieSummaryPresenter {
     private final static MovieSummaryView NULL_VIEW = NullObject.create(MovieSummaryView.class);
 
     private ProgressProvider progressProvider;
-    private String movieID;
+    private String imdbId;
+    private MovieData movieData;
 
-    MovieSummaryPresenter(@NonNull final ProgressProvider progressProvider, final String movieID) {
+    MovieSummaryPresenter(@NonNull final ProgressProvider progressProvider, final MovieData movieData) {
         this.progressProvider = progressProvider;
-        this.movieID = movieID;
+        this.movieData = movieData;
+
     }
 
     public void attach(MovieSummaryView movieSummaryView) {
         // set visibility of the download view here.
         this.movieSummaryView = movieSummaryView;
-        this.progressProvider.registerObserver(movieID, this);
+        imdbId = movieData.getImdbId();
+        this.progressProvider.registerObserver(imdbId, this);
     }
 
     public void detach() {
         this.movieSummaryView = NULL_VIEW;
-        progressProvider.deregisterObserver(movieID, this);
+        progressProvider.deregisterObserver(imdbId, this);
     }
 
     public void present() {
-        int downloadProgress = progressProvider.getDownloadProgress(movieID);
+        int downloadProgress = progressProvider.getDownloadProgress(imdbId);
         movieSummaryView.showProgressBar(downloadProgress);
 
+        Log.d("ID",imdbId);
+        String title = movieData.getTitle();
+        Log.d("TITLE",title);
+
     }
+
+    /*public void onMovieClicked() {
+        movieSummaryView.displayMovieDetail(imdbId);
+        Intent intent = new Intent(this, MovieDetailsActivity.class);
+        intent.putExtra("imdbId", imdbId);
+        startActivity(intent);
+    }*/
 
     public void onProgressUpdated(int progress) {
         movieSummaryView.showProgressBar(progress);
     }
 
     interface MovieSummaryView {
+        //void displayMovieDetail(String imdbId);
         void showProgressBar(int progress);
         void hideProgressBar();
     }
