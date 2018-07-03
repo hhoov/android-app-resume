4cf2c7366f4e75f63a0839a77c863427e0769f05
Hannah Hoover <hannahghoover@gmail.com>
2018-07-03 17:00:46 -0500

    WIP -- the three arrays of strings

--- app/src/main/java/com/example/android/justjava/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/justjava/MovieDetailsActivity.java
@@ -6,53 +6,117 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.justjava.model.MovieDetailData;
+import com.squareup.picasso.Picasso;
 
-public class MovieDetailsActivity extends AppCompatActivity {
+import javax.inject.Inject;
 
-    //@Inject MovieSummaryPresenter movieSummaryPresenter;
+public class MovieDetailsActivity extends AppCompatActivity implements MovieDetailPresenter.MovieDetailView {
 
-    //private ImageView poster;
-    //private TextView rating, votes, id, title, year, rated, released, runtime, genre;
-    //private TextView director, writer, actors, plot, language, country, awards, metascore;
+    @Inject
+    MovieDetailPresenter movieDetailPresenter;
 
-    public MovieDetailsActivity() {
-
-    }
+    private ImageView poster;
+    private TextView rating, votes, id, title, year, rated, released, runtime, genre;
+    private TextView director, writer, actors, plot, language, country, awards, metascore;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //MyApplication.getApplicationComponent().inject(this);
+        MyApplication.getApplicationComponent().inject(this);
         setContentView(R.layout.activity_details);
 
-        TextView id = (TextView) findViewById(R.id.imdbIdTextView);
-        Log.d("ID b4 getIntent() -- ", id.toString());
+        poster = findViewById(R.id.posterImageView);
+        rating = findViewById(R.id.imdbRatingTextView);
+        votes = findViewById(R.id.imdbVotesTextView);
+        id = findViewById(R.id.imdbIdTextView);
+        title = findViewById(R.id.titleTextView);
+        year = findViewById(R.id.yearTextView);
+        rated = findViewById(R.id.ratedTextView);
+        released = findViewById(R.id.releasedTextView);
+        runtime = findViewById(R.id.runtimeTextView);
+        genre = findViewById(R.id.genreTextView);
+        director = findViewById(R.id.directorTextView);
+        writer = findViewById(R.id.writerTextView);
+        actors = findViewById(R.id.actorsTextView);
+        plot = findViewById(R.id.plotTextView);
+        language = findViewById(R.id.languageTextView);
+        country = findViewById(R.id.countryTextView);
+        awards = findViewById(R.id.awardsTextView);
+        metascore = findViewById(R.id.metascoreTextView);
 
         Intent intent = getIntent();
         String action = intent.getAction();
         Uri data = intent.getData();
 
+        String imdbId = intent.getStringExtra("imdbId");
 
-        Log.d("ID AFTER intent -- ", id.toString());
+        //MovieDetailData movieDetailData = (MovieDetailData) intent.getParcelableExtra("imdbId");
 
-        MovieDetailData movieDetailData = (MovieDetailData) intent.getParcelableExtra("detailData");
-
-        id.setText(movieDetailData.getImdbId());
-
-        Log.d("After Parcel -- ", id.toString());
-
-
-
-        //id.setText(movieDetailData.getImdbId());
-        //Log.d(" ID -- ", id.toString());
-
-        //title.setText(movieDetails.getTitle());
-        //id.setText(movieDetails.getImdbId());
+        movieDetailPresenter.attach(this);
+        movieDetailPresenter.present(imdbId);
 
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        movieDetailPresenter.detach();
+    }
+
+    @Override
+    public void showError() {
+        // TODO
+        Toast.makeText(getApplicationContext(), "Oops! Failed to display movie details.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void displayMovieDetails(MovieDetailData movieDetailData) {
+
+        rating.setText(String.valueOf(movieDetailData.getImdbRating()));
+
+        votes.setText(movieDetailData.getImdbVotes());
+        id.setText(movieDetailData.getImdbId());
+        title.setText(movieDetailData.getTitle());
+        year.setText(movieDetailData.getYear());
+        rated.setText(movieDetailData.getRated());
+        released.setText(movieDetailData.getReleased());
+        runtime.setText(movieDetailData.getRuntime());
+
+        genre.setText(movieDetailData.getGenre().toString());
+
+        director.setText(movieDetailData.getDirector());
+        writer.setText(movieDetailData.getWriter());
+
+        actors.setText(movieDetailData.getActors().toString());
+
+        plot.setText(movieDetailData.getPlot());
+
+        language.setText(movieDetailData.getLanguage().toString());
+
+        country.setText(movieDetailData.getCountry());
+        awards.setText(movieDetailData.getAwards());
+        metascore.setText(movieDetailData.getMetascore());
+        // If URL is empty, provide error image
+        if (movieDetailData.getPoster().isEmpty()) {
+            Picasso.get()
+                    .load(R.drawable.error)
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.error)
+                    .into(poster);
+        } else {
+            Picasso
+                    .get()
+                    .load(movieDetailData.getPoster())
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.error)
+                    .resize(500,0)
+                    .centerCrop()
+                    .into(poster);
+        }
+    }
 }
