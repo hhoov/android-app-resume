4ca493a0e8e137e9afc872615030f864ea366e81
Hannah Hoover <hannahghoover@gmail.com>
2018-07-05 14:50:12 -0500

    Cleanup + Json annotations

--- app/src/main/java/com/example/android/justjava/model/MovieDetailData.java
+++ app/src/main/java/com/example/android/justjava/model/MovieDetailData.java
@@ -1,58 +1,52 @@
 package com.example.android.justjava.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MovieDetailData implements Parcelable {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "imdbRating",
+        "imdbVotes",
+        "imdbId",
+        "title",
+        "year",
+        "rated",
+        "released",
+        "runtime",
+        "genre",
+        "director",
+        "writer",
+        "actors",
+        "plot",
+        "language",
+        "country",
+        "awards",
+        "poster",
+        "metascore"
+})
+
+public class MovieDetailData {
+
     private final double imdbRating;
-    private final int imdbVotes;
-    private final String imdbId;
-    private final String title;
-    private final int year;
-    private final String rated;
-    private final String released;
-    private final String runtime;
-
-    private final ArrayList<String> genre;
-
-    private final String director;
-    private final String writer;
-
-    private final ArrayList<String> actors;
-    private final String plot;
-
-    private final ArrayList<String> language;
-
-    private final String country;
-    private final String awards;
-    private final String poster;
-    private final String metascore;
+    private final Integer imdbVotes, year;
+    private final String imdbId, title, rated, released, runtime, director, writer, plot, country, awards, poster, metascore;
+    private List<String> genre;
+    private List<String> actors;
+    private List<String> language;
 
     @JsonCreator
     public MovieDetailData(
-            @JsonProperty("imdbRating") double imdbRating,
-            @JsonProperty("imdbVotes") int imdbVotes,
-            @JsonProperty("imdbId") String imdbId,
-            @JsonProperty("title") String title,
-            @JsonProperty("year") int year,
-            @JsonProperty("rated") String rated,
-            @JsonProperty("released") String released,
-            @JsonProperty("runtime") String runtime,
-            @JsonProperty("genre") ArrayList<String> genre,
-            @JsonProperty("director") String director,
-            @JsonProperty("writer") String writer,
-            @JsonProperty("actors") ArrayList<String> actors,
-            @JsonProperty("plot") String plot,
-            @JsonProperty("language") ArrayList<String> language,
-            @JsonProperty("country") String country,
-            @JsonProperty("awards") String awards,
-            @JsonProperty("poster") String poster,
-            @JsonProperty("metascore") String metascore) {
+            @JsonProperty("imdbRating") double imdbRating, @JsonProperty("imdbVotes") int imdbVotes, @JsonProperty("imdbId") String imdbId,
+            @JsonProperty("title") String title, @JsonProperty("year") int year, @JsonProperty("rated") String rated, @JsonProperty("released") String released,
+            @JsonProperty("runtime") String runtime, @JsonProperty("genre") List<String> genre, @JsonProperty("director") String director, @JsonProperty("writer") String writer,
+            @JsonProperty("actors") List<String> actors, @JsonProperty("plot") String plot, @JsonProperty("language") List<String> language, @JsonProperty("country") String country,
+            @JsonProperty("awards") String awards, @JsonProperty("poster") String poster, @JsonProperty("metascore") String metascore) {
+        super();
         this.imdbRating = imdbRating;
         this.imdbVotes = imdbVotes;
         this.imdbId = imdbId;
@@ -73,139 +67,97 @@ public class MovieDetailData implements Parcelable {
         this.metascore = metascore;
     }
 
-    public MovieDetailData(Parcel source) {
-        imdbRating = source.readDouble();
-        imdbVotes = source.readInt();
-        imdbId = source.readString();
-        title = source.readString();
-        year = source.readInt();
-        rated = source.readString();
-        released = source.readString();
-        runtime = source.readString();
-        genre = source.createStringArrayList();
-        director = source.readString();
-        writer = source.readString();
-        actors = source.createStringArrayList();
-        plot = source.readString();
-        language = source.createStringArrayList();
-        country = source.readString();
-        awards = source.readString();
-        poster = source.readString();
-        metascore = source.readString();
-    }
-
+    @JsonProperty("imdbRating")
     public double getImdbRating() {
         return imdbRating;
     }
 
+    @JsonProperty("imdbVotes")
     public int getImdbVotes() {
         return imdbVotes;
     }
 
+    @JsonProperty("imdbId")
     public String getImdbId() {
         return imdbId;
     }
 
+    @JsonProperty("title")
     public String getTitle() {
         return title;
     }
 
+    @JsonProperty("year")
     public int getYear() {
         return year;
     }
 
+    @JsonProperty("rated")
     public String getRated() {
         return rated;
     }
 
+    @JsonProperty("released")
     public String getReleased() {
         return released;
     }
 
+    @JsonProperty("runtime")
     public String getRuntime() {
         return runtime;
     }
 
-    public ArrayList<String> getGenre(int index) {
+    @JsonProperty("genre")
+    public List<String> getGenre() {
         if (!(genre == null)) return genre;
-        else return new ArrayList<String>();
+        else return new ArrayList<>();
     }
 
+    @JsonProperty("director")
     public String getDirector() {
         return director;
     }
 
+    @JsonProperty("writer")
     public String getWriter() {
         return writer;
     }
 
-    public ArrayList<String> getActors(int index) {
+    @JsonProperty("actors")
+    public List<String> getActors() {
         if (!(actors == null)) return actors;
-        else return new ArrayList<String>();
+        else return new ArrayList<>();
     }
 
+    @JsonProperty("plot")
     public String getPlot() {
         return plot;
     }
 
-    public ArrayList<String> getLanguage(int index) {
+    @JsonProperty("language")
+    public List<String> getLanguage() {
         if (!(language == null)) return language;
-        else return new ArrayList<String>();
+        else return new ArrayList<>();
     }
 
+    @JsonProperty("country")
     public String getCountry() {
         return country;
     }
 
+    @JsonProperty("awards")
     public String getAwards() {
         return awards;
     }
 
+    @JsonProperty("poster")
     public String getPoster() {
         return poster;
     }
 
+    @JsonProperty("metascore")
     public String getMetascore() {
         return metascore;
     }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeDouble(imdbRating);
-        dest.writeInt(imdbVotes);
-        dest.writeString(imdbId);
-        dest.writeString(title);
-        dest.writeInt(year);
-        dest.writeString(rated);
-        dest.writeString(released);
-        dest.writeString(runtime);
-        dest.writeStringList(genre);
-        dest.writeString(director);
-        dest.writeString(writer);
-        dest.writeStringList(actors);
-        dest.writeString(plot);
-        dest.writeStringList(language);
-        dest.writeString(country);
-        dest.writeString(awards);
-        dest.writeString(poster);
-        dest.writeString(metascore);
-    }
-
-    public static final Creator<MovieDetailData> CREATOR = new Creator<MovieDetailData>() {
-        @Override
-        public MovieDetailData createFromParcel(Parcel source) {
-            return new MovieDetailData(source);
-        }
-
-        @Override
-        public MovieDetailData[] newArray(int size) {
-            return new MovieDetailData[size];
-        }
-    };
 
 }
